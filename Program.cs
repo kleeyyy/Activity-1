4a3c019e837f796f315d8c2d301258dec7e42584
kleeyyy <alardemarian@gmail.com>
2025-06-09 13:03:39 +0800

    added another function - CheckLetterType

--- Program.cs
+++ Program.cs
@@ -6,9 +6,30 @@ class Program
     {
         Console.Write("Enter a single letter: ");
         char input = Char.ToLower(Console.ReadKey().KeyChar); 
-        Console.WriteLine(); 
+        Console.WriteLine();
+
+        string result = CheckLetterType(input);
+        Console.WriteLine(result);
 
         Console.WriteLine("Press any key to exit...");
         Console.ReadKey();
     }
+
+    static string CheckLetterType(char letter)
+    {
+        if (!Char.IsLetter(letter))
+        {
+            return "That's not a valid letter.";
+        }
+        else if ("aeiou".Contains(letter))
+        {
+            return $"{letter} is a vowel.";
+        }
+        else
+        {
+            return $"{letter} is a consonant.";
+        }
+    }
 }
+
+    
